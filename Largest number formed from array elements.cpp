93bac0a123dc679be85512e39fbaf85da4d43448
shubham-begn <108439580+shubham-begn@users.noreply.github.com>
2022-09-01 01:41:33 +0530

    Create Largest number formed from array elements.cpp

---
+++ Largest number formed from array elements.cpp
@@ -0,0 +1,55 @@
+gfg link:  https://practice.geeksforgeeks.org/problems/largest-number-formed-from-an-array1117/1?page=2&curated[]=1&sortBy=submissions
+
+
+
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+bool cmp(string x,string y)
+	{
+	    return x+y > y+x;
+	}
+class Solution{
+public:
+	// The main function that returns the arrangement with the largest value as
+	// string.
+	// The function accepts a vector of strings
+	
+	string printLargest(vector<string> &arr) {
+	    
+	    sort(arr.begin(),arr.end(),cmp);
+	    string ans;
+	    
+	    for(int i=0;i<arr.size();i++)
+	    ans+=arr[i];
+	    
+	    return ans;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        vector<string> arr(n);
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.printLargest(arr);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
